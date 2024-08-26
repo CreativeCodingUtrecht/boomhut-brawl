26144d2b9c2a1648ec0a42923264386704d7ac29
Rein van der Woerd <reinvanderwoerd@me.com>
2024-08-26 14:37:39 +0200

    every animation

--- heidag/src/characters/saskia.h
+++ heidag/src/characters/saskia.h
@@ -28,10 +28,16 @@ struct saskia: public character {
     // bn::sprite_item pictogram;
     bn::sprite_ptr sprite_ptr = sprite_item().create_sprite(spawn_point);
 
-    character_animations animations = character_animations {
-        idle: bn::create_sprite_animate_action_forever(sprite_ptr, 1, sprite_item().tiles_item(), 
+
+    // Animations
+    bn::sprite_animate_action<400> idle_anim(bn::sprite_ptr spr) {
+        bn::create_sprite_animate_action_forever(spr, 1, sprite_item().tiles_item(), 
             0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37
-        ),
+        );
+    }
+
+    character_animations animations = character_animations {
+        idle: idle_anim(sprite_ptr),
         run: bn::create_sprite_animate_action_forever(sprite_ptr, 1, sprite_item().tiles_item(), 
             38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55
         ),
