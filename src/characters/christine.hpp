3647326a3f9ae6b588be06582b3330f8f008d0b3
chrs-blip <christinekappe@gmail.com>
2024-09-27 20:35:37 +0000

    change

--- src/characters/christine.hpp
+++ src/characters/christine.hpp
@@ -121,7 +121,7 @@ struct christine: public character {
                 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55
             ),
             .jump_up = bn::create_sprite_animate_action_once(*_sprite_ptr, 1, sprite_item().tiles_item(),
-                56, 57, 58, 59, 60
+               1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
             ),
             .jump_stay = bn::create_sprite_animate_action_forever(*_sprite_ptr, 1, sprite_item().tiles_item(),
                 61, 61
