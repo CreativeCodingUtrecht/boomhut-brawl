3eb12ba65465e4bab0dc457086734324e5507797
Rein van der Woerd <reinvanderwoerd@me.com>
2024-12-13 17:46:34 +0100

    abilitybars position constants

--- src/battle.hpp
+++ src/battle.hpp
@@ -75,20 +75,27 @@ using namespace bn;
 
 struct abilitybars
 {
-    const bn::fixed_point position_1 = bn::fixed_point(-bn::display::width() / 2 + 20, -bn::display::height() / 2 + 30);
-    bn::sprite_ptr abilitybar_fill = bn::sprite_items::abilitybar_fill.create_sprite(position_1.x() + 48, position_1.y()+3);
-    bn::sprite_ptr abilitybar_frame = bn::sprite_items::healthbar_frame.create_sprite(position_1.x() + 48, position_1.y());
+    const int x = 48;
+    const int y = 30;
 
-    const bn::fixed_point position_2 = bn::fixed_point(bn::display::width() / 2 - 20, -bn::display::height() / 2 + 30);
-    const bn::fixed_point abilitybar_fill_position_2 = bn::fixed_point(position_2.x() - 48, position_2.y()+3);
+    const bn::fixed_point position_1 = bn::fixed_point(-bn::display::width() / 2 + 20, -bn::display::height() / 2 + y);
+    bn::sprite_ptr abilitybar_fill = bn::sprite_items::abilitybar_fill.create_sprite(position_1.x() + x, position_1.y()+3);
+    bn::sprite_ptr abilitybar_frame = bn::sprite_items::healthbar_frame.create_sprite(position_1.x() + x, position_1.y());
+
+    const bn::fixed_point position_2 = bn::fixed_point(bn::display::width() / 2 - 20, -bn::display::height() / 2 + y);
+    const bn::fixed_point abilitybar_fill_position_2 = bn::fixed_point(position_2.x() - x, position_2.y()+3);
     bn::sprite_ptr abilitybar_fill2 = bn::sprite_items::abilitybar_fill.create_sprite(abilitybar_fill_position_2);
-    bn::sprite_ptr abilitybar_frame2 = bn::sprite_items::healthbar_frame.create_sprite(position_2.x() - 48, position_2.y());
+    bn::sprite_ptr abilitybar_frame2 = bn::sprite_items::healthbar_frame.create_sprite(position_2.x() - x, position_2.y());
 
     abilitybars()
     {
         abilitybar_frame2.set_horizontal_flip(true);
         abilitybar_fill2.set_horizontal_flip(true);
 
+        // abilitybar_frame.set_vertical_scale(0.5);
+        // abilitybar_fill.set_vertical_scale(0.6);
+        // abilitybar_frame2.set_vertical_scale(0.5);
+        // abilitybar_fill2.set_vertical_scale(0.6);
     }
 
     void set_ability_left(bn::fixed ability)
@@ -272,7 +279,7 @@ namespace battle
 
         // Health bars
         healthbars bars = healthbars(you->avatar(), other_player->avatar());        
-        // abilitybars abilitybars;
+        abilitybars abilitybars;
 
         // Camera
         you->sprite_ptr()->set_camera(*camera);
@@ -367,8 +374,8 @@ namespace battle
             you->increase_ability();
             other_player->increase_ability();
 
-            // abilitybars.set_ability_left(you->get_ability() / you->max_ability);
-            // abilitybars.set_ability_right(other_player->get_ability() / other_player->max_ability);        
+            abilitybars.set_ability_left(you->get_ability() / you->max_ability);
+            abilitybars.set_ability_right(other_player->get_ability() / other_player->max_ability);        
 
             // Back to character select
             if (bn::keypad::select_pressed() || multiplayer::other_player_keypad_data.keypad_data.select_pressed) {
