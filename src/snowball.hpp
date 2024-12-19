ca38a15949788f40a70a5408ba636d6bcf4bc086
Rein van der Woerd <reinvanderwoerd@me.com>
2024-12-19 22:51:10 +0100

    add snowball skeleton file

--- src/snowball.hpp
+++ src/snowball.hpp
@@ -1,6 +1,17 @@
 #pragma once
 
+#include "bn_vector.h"
+#include "bn_fixed_point.h"
+
 
 struct snowball {
+    bn::fixed_point position;
+    bn::fixed direction_x;
 
+    void update() {
+        
+    }
 };
+
+
+bn::vector<snowball, 20> snowballs;
